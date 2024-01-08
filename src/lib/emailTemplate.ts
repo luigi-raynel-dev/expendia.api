84a6d585a335e5366959606e70f4a04709f5f106
Luigi Raynel <74196979+luigi-raynel-dev@users.noreply.github.com>
2024-01-08 08:25:14 -0300

    fix: remove title from email template

--- src/lib/emailTemplate.ts
+++ src/lib/emailTemplate.ts
@@ -15,7 +15,6 @@ export function emailTemplate(
     <div style="margin: 0 auto; width: 100%; max-width: 800px; border: 1px solid #ddd">
       <div style="background: #A34FD8; width: 100%; max-width: 800px; text-align: center; margin: 0 auto; padding: 8px; color: white">
         <img width="200px" src="https://expendia.netlify.app/static/media/ExpendiaLogo.bf7c0010270a7a3a8267.png" />
-        <h1 style="font-size: 25px">${title}</h1>
       </div>
       <div style="padding: 8px">
         <h3>${helloLabel || `Olá`} ${username},</h3>
