059e50132b86661a0195deec08fd21400e4576e4
Luigi Raynel <74196979+luigi-raynel-dev@users.noreply.github.com>
2024-01-17 09:13:46 -0300

    feat: invalid jwt if the user doesn't exist

--- src/plugins/authenticate.ts
+++ src/plugins/authenticate.ts
@@ -1,5 +1,22 @@
-import { FastifyRequest } from 'fastify'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { prisma } from '../lib/prisma'
 
-export async function authenticate(request: FastifyRequest) {
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
   await request.jwtVerify()
+
+  const { sub: user_id } = request.user
+
+  const user = await prisma.user.findUnique({
+    where: { id: user_id }
+  })
+
+  if (!user)
+    return reply.status(401).send({
+      status: false,
+      error: 'Unauthorized',
+      message: "The user doesn't exist in our database"
+    })
 }
