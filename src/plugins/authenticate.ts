e6e768217bf813b21112b70d67e52ae2237696ef
Luigi Raynel <74196979+luigi-raynel-dev@users.noreply.github.com>
2024-01-26 09:20:21 -0300

    feat: send 401 status when user not exists in DB

--- src/plugins/authenticate.ts
+++ src/plugins/authenticate.ts
@@ -1,22 +1,6 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { prisma } from '../lib/prisma'
 
-export async function authenticate(
-  request: FastifyRequest,
-  reply: FastifyReply
-) {
+export async function authenticate(request: FastifyRequest) {
   await request.jwtVerify()
-
-  const { sub: user_id } = request.user
-
-  const user = await prisma.user.findUnique({
-    where: { id: user_id }
-  })
-
-  if (!user)
-    return reply.status(401).send({
-      status: false,
-      error: 'Unauthorized',
-      message: "The user doesn't exist in our database"
-    })
 }
